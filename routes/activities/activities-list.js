b187576b9b8812c12363772d02a50706918fbdf8
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-16 12:53:20 +0200

    Added activities route

---
+++ routes/activities/activities-list.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const managers = require('../../managers');
+
+/* GET activities */
+router.get('/activities', async (req, res, next) => {
+  let result = {};
+  try {
+    result = { activities: await managers.activities.getActivities() };
+    res.status(200);
+  } catch (e) {
+    res.status(500);
+  } finally {
+    res.json(result);
+  }
+});
+
+module.exports = router;
