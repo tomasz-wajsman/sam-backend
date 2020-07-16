25026ebd04143a42ae37f698b58a6d1556f71e94
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-16 12:53:50 +0200

    Added database

--- test/suites/api/activities/activities.test.js
+++ test/suites/api/activities/activities.test.js
@@ -1,9 +1,32 @@
+/* eslint-disable no-console */
 const request = require('supertest');
 const assert = require('assert');
 
 const app = require('../../../../app');
 
+let imsConnection;
+let imsUrl;
+let mongooseClient;
+
+const MongooseClient = require('../../../../clients/database/MongooseClient');
+const inMemoryServer = require('../../../db/mongo');
+
+const models = require('../../../../database/mongo/models');
+const mockedActivities = require('../../../mocks/data/activities.json');
+
 describe('Activity endpoints tests', () => {
+  beforeAll(async () => {
+    try {
+      // try connecting to the server
+      imsUrl = await inMemoryServer.getConnectionString();
+      imsConnection = await inMemoryServer.connect();
+      mongooseClient = new MongooseClient(imsConnection);
+      // insert the data
+      await models.Activity.insertMany(mockedActivities);
+    } catch (ex) {
+      console.error(ex);
+    }
+  });
   test('Returns the list of activities', (done) => {
     request(app)
       .get('/activities')
@@ -17,8 +40,17 @@ describe('Activity endpoints tests', () => {
         const activities = res.body.activities;
         assert.equal(Array.isArray(activities), true, 'Activities were not received');
         assert.equal(activities.length > 0, true, 'There are no activities');
+        assert.equal(activities.length, mockedActivities.length, 'Wrong activities count');
         done();
       });
+  });
+  afterAll(async () => {
+    try {
+      await inMemoryServer.disconnect();
+      await mongooseClient.disconnect();
+    } catch (ex) {
+      console.error(ex);
+    }
   });
   /*
   test('Returns 404 on unknown path', (done) => {
