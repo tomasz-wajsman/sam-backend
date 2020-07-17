42915e073fb0cb259632e4161ad4e8e1aa72d849
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-17 12:58:38 +0200

    Added checking activity parameters

--- test/suites/api/activities/activities.invalid.test.js
+++ test/suites/api/activities/activities.invalid.test.js
@@ -11,13 +11,49 @@ const invalidIds = [
   'zzzzzzzzzzzzzzzzzzzzzzzz'
 ];
 
+const invalidActivities = [
+  { name: 'name' },
+  { name: 'name', category: 'category' },
+  { name: 'name', category: 'category', start_date: 1 },
+  { category: 'category', start_date: 1, end_date: 2 },
+  { distance: 1 },
+  { start_date: 1, end_date: 2, distance: 1 },
+  { start_date: 1, end_date: 1 },
+  { name: '', category: '', start_date: 1, end_date: 2, distance: 1 },
+  { name: 'name', category: 'category', start_date: 1000, end_date: 1000, distance: 1 },
+  { name: 'name', category: 'category', start_date: 1000, end_date: 999, distance: 1 },
+  { name: 'name', category: 'category', start_date: 1, end_date: 2, distance: -1000 }
+];
+
 describe('Activities endpoints invalid use tests', () => {
-  test('Returns 400 on incorrect IDs', (done) => {
+  test('Returns 400 on incorrect IDs for getting single activities', (done) => {
     const promises = [];
     invalidIds.forEach(id => {
       promises.push(
         request(app)
           .get(`/activities/${id}`)
+          .expect('Content-Type', /json/)
+          .expect(400)
+      );
+    });
+    Promise.all(promises)
+      .then(() => done())
+      .catch(e => done(e));
+  });
+  test('Returns 400 on incorrect details for creating activities', (done) => {
+    const promises = [];
+    invalidActivities.forEach(activity => {
+      const item = {
+        name: activity.name,
+        category: activity.category,
+        start_date: activity.start_date,
+        end_date: activity.end_date,
+        distance: activity.distance
+      };
+      promises.push(
+        request(app)
+          .post('/activities')
+          .send({ activity: item })
           .expect('Content-Type', /json/)
           .expect(400)
       );
--- util/request/params.js
+++ util/request/params.js
@@ -8,17 +8,84 @@ const checkActivityID = (req, res, next) => {
     next();
   } else {
     res.status(400);
-    res.json({ message: 'Incorrect activity ID' });
+    res.json({ messages: ['Missing or incorrect activity ID'] });
   }
 };
 
 const checkActivityDetails = (req, res, next) => {
+  let correct = true;
   const body = req.body;
   // check if activity has sufficient and correct parameters
   const activity = body.activity;
 
-  res.locals.activity = activity;
-  next();
+  const messages = [];
+
+  // activity name
+  const name = activity.name;
+  if (!name) {
+    correct = false;
+    messages.push('Activity name was not provided');
+  } else if (name === '') {
+    correct = false;
+    messages.push('Activity name is empty');
+  }
+
+  // category name
+  const category = activity.category;
+  if (!category) {
+    correct = false;
+    messages.push('Activity category was not provided');
+  } else if (category === '') {
+    correct = false;
+    messages.push('Activity category is empty');
+  }
+
+  // start date
+  const startDate = activity.start_date;
+  if (!startDate) {
+    correct = false;
+    messages.push('Start date was not provided');
+  } else if (Date.parse(startDate) < 0) {
+    correct = false;
+    messages.push('Start date is negative');
+  }
+
+  // end date
+  const endDate = activity.end_date;
+  if (!endDate) {
+    correct = false;
+    messages.push('End date was not provided');
+  } else if (Date.parse(endDate) < 0) {
+    correct = false;
+    messages.push('End date is negative');
+  }
+
+  // check dates
+  if (Date.parse(startDate) >= Date.parse(endDate)) {
+    correct = false;
+    messages.push('The start date is same or after end date');
+  }
+
+  // distance if provided
+  const distance = activity.distance;
+  if (distance) {
+    const distanceNum = Number.parseFloat(distance);
+    if (Number.isNaN(distanceNum)) {
+      correct = false;
+      messages.push('Incorrect distance');
+    } else if (distanceNum <= 0) {
+      correct = false;
+      messages.push('Zero or negative distance');
+    }
+  }
+
+  if (correct) {
+    res.locals.activity = activity;
+    next();
+  } else {
+    res.status(400);
+    res.json({ messages });
+  }
 };
 
 module.exports = {
